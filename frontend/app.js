32a1b07b6d6fea402a87a64b6aa708f0c85ae0e5
ll3u <76758163+ll3u@users.noreply.github.com>
2026-07-03 23:38:24 +0200

    fix: wrong config used to keep suffixes

--- frontend/app.js
+++ frontend/app.js
@@ -247,7 +247,7 @@ function renderChordSheet() {
         sharpSong.metadata.set('subtitle', ''); 
         sharpSong.metadata.set('album', ''); 
 
-        const divFormatter = new ChordSheetJS.HtmlDivFormatter({ normalizeChords: false });
+        const divFormatter = new ChordSheetJS.HtmlDivFormatter({ normalizeChordSuffix: false });
         let mainBodyHtml = divFormatter.format(sharpSong);
 
        //mainBodyHtml = mainBodyHtml.replace(/<div class="chord">B([b#]?)([^<]*)<\/div>/g, (match, accidental, rest) => {
